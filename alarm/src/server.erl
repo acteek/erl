9887e53514c82abbb95c08cfefe070168f91caf7
Sergey Ryazanov <acteek@ya.ru>
2017-05-18 01:05:22 +0300

    update-server

--- alarm/src/server.erl
+++ alarm/src/server.erl
@@ -31,19 +31,71 @@ loop(S = #state{} )  ->
       Pid ! {MsgRef, ok},
       loop(S#state{clients = NewClient});
 
-    {Pid, MsgRef, {add, Name, Desctiption, TimeOut}} ->
-      ok;
+    {Pid, MsgRef, {add, Name, Description, TimeOut}} ->
+      case valid_datetime(TimeOut) of
+        true ->
+          EventPid = event:start_link(Name, TimeOut),
+          NewEvents = add_events(Name, Description, EventPid, TimeOut, S),
+      Pid ! {MsgRef, ok},
+          loop(S#state{events = NewEvents});
+        false ->
+          Pid ! {MsgRef, {error, bad_timeout}},
+          loop(S)
+      end;
     {Pid, MsgRef, {cancel, Name}} ->
-      ok;
+      Events = case orddict:find(Name, S#state.events) of
+                 {ok, E} ->
+                   event:cancel(E#event.pid),
+                   orddict:erase(Name, S#state.events);
+                 error ->
+                   S#state.events
+               end,
+      Pid ! {MsgRef, ok},
+      loop(S#state{events = Events});
     {done, Name} ->
-      ok;
+      case orddict:find(Name, S#state.events) of
+        {ok, E} ->
+          send_to_clients({done, E#event.name, E#event.description}, S#state.clients),
+          NewEvents = orddict:erase(Name, S#state.events),
+          loop(S#{state.events = NewEvents});
+        error ->
+          loop(S)
+      end;
     shutdown ->
-      ok;
-    { 'DOWN' , Ref , process , _Pid , _Reason} ->
-      ok;
+      exit(shutdown);
+    {'DOWN' , Ref , process , _Pid , _Reason} ->
+      loop(S#state{clients = orddict:erase(Ref, S#state.clients)});
     code_change ->
-      ok;
+      ?MODULE:loop(S);
     Unknown ->
       io:format("Unknown message:~p~n",[Unknown]),
       loop(S)
   end.
+
+
+send_to_clients(Msg, ClientDict) ->
+  orddict:map(fun(_, Pid) -> Pid ! Msg end, ClientDict).
+
+add_events(Name, Description, Pid, TimeOut, State) ->
+  orddict:store(Name, #event{name = Name,
+    description = Description,
+    pid = Pid,
+    timeout = TimeOut},
+    State#state.events
+  ).
+
+
+valid_time({H, M, S}) -> valid_time(H, M, S).
+valid_time(H, M, S ) when H >= 0 , H < 24 ,
+                           M >= 0 , M < 60 ,
+                           S >= 0 , S < 60 -> true ;
+valid_time(_, _,_) -> false.
+
+
+valid_datetime({Date, Time}) ->
+  try
+    calendar:valid_date(Date) andalso valid_time(Time)
+  catch
+    error:function_clause -> false
+  end;
+valid_datetime(_) -> false.
